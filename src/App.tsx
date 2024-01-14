47341952149836a6fe7034da8d3cd3b8830bf236
Sayd Karam <onltrabalho@gmail.com>
2024-01-14 02:09:21 -0300

    style: :art: create routes for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Router from "./routes/Router"
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Router></Router>
+    </>
+  )
 }
 
 export default App
---
+++ src/routes/Router.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+
+import Login from "../pages/Login"
+import Home from "../pages/Home"
+import NewAnnotation from "../pages/NewAnnotation"
+import Register from "../pages/Register"
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/newAnnotation" element={<NewAnnotation />} />
+        <Route path="/Salvo" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
