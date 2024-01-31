8905f9ac6ce31207ea68599ea1b908dc43c46f02
Sayd Karam <onltrabalho@gmail.com>
2024-01-30 22:13:01 -0300

    style: :art: Change image and structure login

--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,3 +1,18 @@
 import { styled } from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+export const ContainerContent = styled.div`
+
+
+`
+export const ContainerLeft = styled.div`
+width: 50%;
+height: 100vh;
+`
+export const ContainerRight = styled.div`
+width: 50%;
+height: 100vh;
+`
