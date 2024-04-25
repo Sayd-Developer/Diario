acd18592b18f9a40ee143d1e6450956546894a92
Sayd Karam <onltrabalho@gmail.com>
2024-04-25 05:14:58 -0300

    style: :art: create input Login

---
+++ src/components/InputLogin/input.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { InputCustomizado } from "./style";
+
+interface InputProps {
+    type: string;
+    text: string;
+    register: () => void;
+    maxLength: number;
+}
+
+const Input: React.FC<InputProps> = ({ type, text, register, maxLength }) => {
+    return (
+        <InputCustomizado type={type} placeholder={text} ref={register} maxLength={maxLength} />
+    );
+};
+
+export default Input;
---
+++ src/components/InputLogin/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const InputCustomizado = styled.input`
+  padding: 10px;
+  border: none;
+  width: 100%;
+  outline: none;
+  background-color: transparent;
+`
