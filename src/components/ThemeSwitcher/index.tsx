c54adf2b107055a64e1bc03939400f4eb1b47663
Sayd Karam <onltrabalho@gmail.com>
2024-04-25 05:12:50 -0300

    style: :art: change theme

---
+++ src/components/ThemeSwitcher/index.tsx
@@ -0,0 +1,11 @@
+interface ThemeSwitcherProps {
+    toggleTheme: () => void;
+}
+
+const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ toggleTheme }) => {
+    return (
+        <button onClick={toggleTheme} >Toggle Theme</button>
+    );
+};
+
+export default ThemeSwitcher;
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,28 +1,26 @@
 // Definição de tipagem
 
 export interface Props {
-    primary: string,
-    secondary: string,
+  primary: string
+  secondary: string
 
-    primaryText: string,
-    secondaryText: string
+  primaryText: string
+  secondaryText: string
 }
 
-// Definição de Temas 
+// Definição de Temas
 
 export const ligthTheme = {
-    primary: '#fff',
-    secondary: '#E0E0E0',
+  primary: "#fff",
+  secondary: "#E0E0E0",
 
-    primaryText: '#fff',
-    secondaryText: '#E0E0E0',
-
+  primaryText: "#fff",
+  secondaryText: "#E0E0E0",
 }
 export const darkTheme = {
-    primary: '#131313',
-    secondary: '#fff',
+  primary: "#131313",
+  secondary: "#fff",
 
-    primaryText: '#fff',
-    secondaryText: '#131313',
-
+  primaryText: "#fff",
+  secondaryText: "#131313",
 }
