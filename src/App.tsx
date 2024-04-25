7a7fdff3777417fff144f7f066bb988ba9f209e7
Sayd Karam <onltrabalho@gmail.com>
2024-04-25 05:18:24 -0300

    refactor: :art: change app, add new things

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
+import NavigationRoutes from "./routes";
+import { GlobalStyles } from "./style/GlobalStyles";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { ligthTheme, darkTheme } from "../src/theme/theme";
+import ThemeSwitcher from "../src/components/ThemeSwitcher";
+import { SidebarProvider } from "./Context/SideBarContext";
 
-import NavigationRoutes from "./routes"
-import { GlobalStyles } from "./style/GlobalStyles"
-import { BrowserRouter } from "react-router-dom"
-import { ThemeProvider } from "styled-components"
+export default function App() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
-import { ligthTheme, darkTheme } from "../src/theme/theme"
-
-function App() {
-
-  const [theme, setTheme] = useState("dark ")
-
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light")
-  }
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
+  };
 
   return (
-    <>
+    <BrowserRouter>
       <ThemeProvider theme={theme === "light" ? ligthTheme : darkTheme}>
-        <BrowserRouter>
-          <GlobalStyles />
+        <SidebarProvider>
+          <ThemeSwitcher toggleTheme={toggleTheme} />
           <NavigationRoutes />
-          <button onClick={() => themeToggler()}>IHdbabsdaiusdv</button>
-        </BrowserRouter>
+          <GlobalStyles />
+        </SidebarProvider>
       </ThemeProvider>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
-
-export default App
