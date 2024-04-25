9b63a78754c089b481c1f222a7290bea6c81fde8
Sayd Karam <onltrabalho@gmail.com>
2024-04-25 05:13:50 -0300

    style: :art: change routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,11 @@ import { Suspense } from "react";
 import { useRoutes } from "react-router-dom";
 import { routes } from "./Router";
 
-const LoadingFallback = () => <div></div>;
+// const LoadingFallback = () => <div></div>;
+// fallback = {< LoadingFallback />
 
 export default function NavigationRoutes() {
     return (
-        <Suspense fallback={<LoadingFallback />}>{useRoutes(routes)}</Suspense>
+        <Suspense >{useRoutes(routes)}</Suspense>
     );
 }
