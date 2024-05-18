88534c9611aa08a0944db6751cdb5e79e2b71ddc
Sayd Karam <onltrabalho@gmail.com>
2024-05-18 03:09:08 -0300

    refactor: :art: delete this pages

---
+++ src/components/InputAreaRegister/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const InputAreaPersonalizado = styled.textarea`
+  box-sizing: border-box;
+  background: #131313;
+  border-radius: 5px;
+  color: #fff;
+  resize: none;
+  height: 450px;
+  padding: 10px;
+`
